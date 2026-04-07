477973802679ac089f8f33d8002497e84609a1c1
Ben Donaghy <debnon@Bens-MacBook-Air.local>
2026-04-07 20:42:04 +0100

    init

---
+++ StandardUser.py
@@ -0,0 +1,17 @@
+class StandardUser:
+    """"User class"""
+
+    name: str = "example"
+    funds: int = 0
+
+    def __init__(self, name: str = name):
+        self.name = name
+
+    def getName(self):
+        return greetings
+
+    # def __init__(self):
+    #     return
+    
+    # def User():
+    #     print("Hey")
